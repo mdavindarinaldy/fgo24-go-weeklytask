02e8a0adf0a95ea1b6ec88c4d92634e82536785c
mdavindarinaldy <153896812+mdavindarinaldy@users.noreply.github.com>
2025-06-13 00:09:18 +0700

    feat: add set time out (time.Sleep) to wait for a second after appending item to cart

--- module/shopping_cart.go
+++ module/shopping_cart.go
@@ -1,9 +1,16 @@
 package module
 
+import (
+	"fmt"
+	"time"
+)
+
 var Cart []Item = []Item{}
 
 func AddCartItem(item Item) {
 	Cart = append(Cart, item)
+	fmt.Printf("\n%s berhasil ditambahkan ke dalam keranjang!\n\n", item.Name)
+	time.Sleep(time.Second)
 }
 
 // implementasi goroutines : lakukan pencarian berdasarkan range harga, rating, kategori --> masukkan ke dalam
