df14d51898955e454e646ca50d632b4b93bc7a08
mdavindarinaldy <153896812+mdavindarinaldy@users.noreply.github.com>
2025-06-12 16:43:58 +0700

    feat: add func to handle invalid input

--- utils/input.go
+++ utils/input.go
@@ -1,10 +1,18 @@
 package utils
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
 
 func GetInputInt(prompt string) int {
 	fmt.Print(prompt)
 	var input int
 	fmt.Scanln(&input)
 	return input
+}
+
+func InvalidInput() {
+	fmt.Print("\nInput tidak valid! Silakan coba lagi\n\n")
+	time.Sleep(time.Second)
 }
