54bd0cea376b6768153074b3c3534651e16e52d5
mdavindarinaldy <153896812+mdavindarinaldy@users.noreply.github.com>
2025-06-12 16:43:40 +0700

    feat: implementing goroutines

--- main.go
+++ main.go
@@ -5,9 +5,17 @@ import (
 	"fgo24-weekly-go/utils"
 	"fmt"
 	"os"
+	"sync"
 )
 
 func main() {
+	wait := sync.WaitGroup{}
+	wait.Add(4)
+	go menu.FoodItem(&wait)
+	go menu.DrinkItem(&wait)
+	go menu.SnackItem(&wait)
+	go menu.DessertItem(&wait)
+	wait.Wait()
 	for {
 		menu.MainMenu()
 		option := utils.GetInputInt("Pilih Menu 1-4: ")
@@ -20,6 +28,8 @@ func main() {
 		} else if option == 4 {
 			fmt.Println("Terima kasih sudah berkunjung!")
 			os.Exit(0)
+		} else {
+			utils.InvalidInput()
 		}
 	}
 }
--- menu/shopping_cart.go
+++ menu/shopping_cart.go
@@ -1,95 +1,146 @@
 package menu
 
+import "sync"
+
 type Item struct {
-	name   string
-	price  int
-	origin string
+	name     string
+	price    int
+	origin   string
+	category string
 }
 
-var FoodItems []Item = []Item{
+var Items []Item = []Item{
 	{
-		name:   "Nasi Goreng",
-		price:  15000,
-		origin: "Nusantara",
+		name:     "Nasi Goreng",
+		price:    15000,
+		origin:   "Nusantara",
+		category: "Food",
 	}, {
-		name:   "Nasi Uduk",
-		price:  10000,
-		origin: "Nusantara",
+		name:     "Nasi Uduk",
+		price:    10000,
+		origin:   "Nusantara",
+		category: "Food",
 	}, {
-		name:   "Fish and Chips",
-		price:  18000,
-		origin: "Western",
+		name:     "Fish and Chips",
+		price:    18000,
+		origin:   "Western",
+		category: "Food",
 	}, {
-		name:   "Sushi",
-		price:  20000,
-		origin: "Japanese",
+		name:     "Sushi",
+		price:    20000,
+		origin:   "Japanese",
+		category: "Food",
+	}, {
+		name:     "Es Dawet",
+		price:    9000,
+		origin:   "Nusantara",
+		category: "Drink",
+	}, {
+		name:     "Cherry Blossom Tea",
+		price:    5000,
+		origin:   "Japanese",
+		category: "Drink",
+	}, {
+		name:     "Kombucha",
+		price:    8000,
+		origin:   "Japanese",
+		category: "Drink",
+	}, {
+		name:     "Jasmine Tea in Affogato Latte",
+		price:    13000,
+		origin:   "Western",
+		category: "Drink",
+	}, {
+		name:     "Mochi",
+		price:    9000,
+		origin:   "Japanese",
+		category: "Snack",
+	}, {
+		name:     "Dango",
+		price:    10000,
+		origin:   "Japanese",
+		category: "Snack",
+	}, {
+		name:     "Pisang Goreng",
+		price:    5000,
+		origin:   "Nusantara",
+		category: "Snack",
+	}, {
+		name:     "Serabi",
+		price:    7500,
+		origin:   "Nusantara",
+		category: "Snack",
+	}, {
+		name:     "Cheesecake",
+		price:    20000,
+		origin:   "Western",
+		category: "Dessert",
+	}, {
+		name:     "Red Velvet Cake",
+		price:    22000,
+		origin:   "Western",
+		category: "Dessert",
+	}, {
+		name:     "Creme Brulee",
+		price:    18000,
+		origin:   "Western",
+		category: "Dessert",
+	}, {
+		name:     "Bubur Kacang Ijo",
+		price:    10000,
+		origin:   "Nusantara",
+		category: "Dessert",
 	},
 }
 
-var DrinkItems []Item = []Item{
-	{
-		name:   "Es Dawet",
-		price:  9000,
-		origin: "Nusantara",
-	}, {
-		name:   "Cherry Blossom Tea",
-		price:  5000,
-		origin: "Japanese",
-	}, {
-		name:   "Kombucha",
-		price:  8000,
-		origin: "Japanese",
-	}, {
-		name:   "Jasmine Tea in Affogato Latte",
-		price:  13000,
-		origin: "Western",
-	},
-}
+var FoodItems []Item = []Item{}
 
-var SnackItems []Item = []Item{
-	{
-		name:   "Mochi",
-		price:  9000,
-		origin: "Japanese",
-	}, {
-		name:   "Dango",
-		price:  10000,
-		origin: "Japanese",
-	}, {
-		name:   "Pisang Goreng",
-		price:  5000,
-		origin: "Nusantara",
-	}, {
-		name:   "Serabi",
-		price:  7500,
-		origin: "Nusantara",
-	},
-}
+var DrinkItems []Item = []Item{}
 
-var DessertItems []Item = []Item{
-	{
-		name:   "Cheesecake",
-		price:  20000,
-		origin: "Western",
-	}, {
-		name:   "Red Velvet Cake",
-		price:  22000,
-		origin: "Western",
-	}, {
-		name:   "Creme Brulee",
-		price:  18000,
-		origin: "Western",
-	}, {
-		name:   "Bubur Kacang Ijo",
-		price:  10000,
-		origin: "Nusantara",
-	},
-}
+var SnackItems []Item = []Item{}
+
+var DessertItems []Item = []Item{}
 
 var Cart []Item
 
 func AddItem(item Item) {
 	Cart = append(Cart, item)
+}
+
+func FoodItem(wg *sync.WaitGroup) {
+	defer wg.Done()
+	for _, item := range Items {
+		if item.category == "Food" {
+			FoodItems = append(FoodItems, item)
+		}
+	}
+}
+
+func DrinkItem(wg *sync.WaitGroup) {
+	defer wg.Done()
+	for _, item := range Items {
+		if item.category == "Food" {
+			DrinkItems = append(DrinkItems, item)
+		}
+	}
+}
+
+func SnackItem(wg *sync.WaitGroup) {
+	defer wg.Done()
+	for _, item := range Items {
+		if item.category == "Food" {
+			SnackItems = append(SnackItems, item)
+		}
+	}
+}
+
+func DessertItem(wg *sync.WaitGroup) {
+	defer wg.Done()
+	for _, item := range Items {
+		if item.category == "Food" {
+			DessertItems = append(DessertItems, item)
+		}
+	}
 }
 
 // implementasi goroutines : lakukan pencarian berdasarkan range harga, rating, kategori --> masukkan ke dalam
